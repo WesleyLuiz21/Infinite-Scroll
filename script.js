4384a9ed24d33cbff39538f6bb5ecd896b5ece62
unknown <wesley_luiz2001@hotmail.com>
2023-02-15 16:22:31 +0000

    reduced the repetition in the script file and moving to the scrolling function

--- script.js
+++ script.js
@@ -22,13 +22,19 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // creating an <a> to link to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         // create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
         // put <img> inside <a> then put both inside imgContainer Element
         item.appendChild(img);
         imgContainer.appendChild(item);
@@ -46,6 +52,8 @@ async function getPhotos() {
 
     }
 }
+
+// Check to see if Scrolling near bottom of page and load more photos
 
 // On load
 getPhotos();
