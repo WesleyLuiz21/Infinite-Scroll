6f79f4c04a9924f643fb584794c065d4db9da261
unknown <wesley_luiz2001@hotmail.com>
2023-02-20 16:13:46 +0000

    added more functions and logic to make the app work as intended

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const imgContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
-let photosArray = [];
+let photosArray = []; 
 
+let ready = false;
+let imgsLoaded = 0;
+let totalImgs = 0;
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'VNZpDoTQEUSPnq7HfA8iPtEp-lJmrw8DsdMANlMJBQQ';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all imgs were loaded
+function imgLoaded() {
+
+}
 
 // Helper Function to set attributes on DOM Elements
 
@@ -35,6 +43,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        // Event Listener, check when each  is finished loading
+        img.addEventListener('load', imgLoaded);
         // put <img> inside <a> then put both inside imgContainer Element
         item.appendChild(img);
         imgContainer.appendChild(item);
@@ -54,6 +64,11 @@ async function getPhotos() {
 }
 
 // Check to see if Scrolling near bottom of page and load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos();
+    }
+});
 
 // On load
 getPhotos();
